dd3d2f3aff341d861f0df36e80f3407491d72d6f
Nessim Dridi <nessim.dridi@gmx.net>
2023-11-27 10:12:39 +0100

    Initial Commit of the first Version of QuantumDroneTrafficManagement. Contains a simulation of two drones on a 2 by 2 grid.

---
+++ QuantumDroneTrafficManagement_2by2/QuantumDroneTrafficManagement_2by2.py
@@ -0,0 +1,78 @@
+import random
+
+class DroneGrid:
+    def __init__(self, rows, cols):
+        self.rows = rows
+        self.cols = cols
+        self.grid = [[' ' for _ in range(cols)] for _ in range(rows)]
+        self.drones = {}
+
+    def place_drone(self, drone, row, col):
+        if 0 <= row < self.rows and 0 <= col < self.cols and self.grid[row][col] == ' ':
+            if drone.name in self.drones:
+                prev_row, prev_col = self.drones[drone.name].position
+                self.grid[prev_row][prev_col] = ' '
+            self.grid[row][col] = drone.name
+            drone.place(row, col)
+            self.drones[drone.name] = drone
+            return True
+        else:
+            print(f"Invalid position for drone {drone.name}")
+            return False
+
+    def move_drone(self, drone, move_type, amount):
+        if drone.name in self.drones:
+            current_row, current_col = self.drones[drone.name].position
+            new_position = drone.move(move_type, amount)
+
+            if new_position is not False:
+                new_row, new_col = new_position
+                if 0 <= new_row < self.rows and 0 <= new_col < self.cols and self.grid[new_row][new_col] == ' ':
+                    self.grid[current_row][current_col] = ' '
+                    self.grid[new_row][new_col] = drone.name
+                    drone.place(new_row, new_col)
+                    return True
+                else:
+                    print(f"Invalid move for drone {drone.name}")
+                    return False
+            else:
+                return False
+        else:
+            print(f"Drone {drone.name} not found")
+            return False
+
+    def display_grid(self):
+        for row in self.grid:
+            print('[{}] [{}]'.format(*row))
+
+    def is_drone(self, row, col):
+        return self.grid[row][col] != ' ' if 0 <= row < self.rows and 0 <= col < self.cols else False
+
+class Drone:
+    def __init__(self, name):
+        self.name = name
+        self.position = None
+        self.last_move_direction = None  # New attribute to track the last move direction
+
+    def place(self, row, col):
+        self.position = (row, col)
+
+    def move(self, move_type, amount):
+        row, col = self.position
+
+        if move_type == 'vertical':
+            row = (row + amount) % 2  # Wrap around for a 2x2 grid
+        elif move_type == 'horizontal':
+            col = (col + amount) % 2  # Wrap around for a 2x2 grid
+        else:
+            print("Invalid move type")
+            return False
+
+        self.last_move_direction = move_type  # Update the last move direction
+        return row, col
+
+    def get_position(self):
+        return self.position
+
+    def get_last_move(self):
+        return self.last_move_direction
---
+++ QuantumDroneTrafficManagement_2by2/main_2by2.py
@@ -0,0 +1,68 @@
+import os
+import time
+import random
+from DroneTrafficManagement_2by2 import Drone, DroneGrid
+import perceval as pcvl
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def move_drones(grid, drones, sampler):
+    sample_count = sampler.sample_count(1)
+    counts = sample_count['results']
+    
+    if counts[pcvl.BasicState([0,1,1,0])]==1:
+        moveA = 'horizontal'
+        moveB = 'horizontal'
+    if counts[pcvl.BasicState([1,0,1,0])]==1:
+        moveA = 'vertical'
+        moveB = 'horizontal'
+    if counts[pcvl.BasicState([1,0,0,1])]==1:
+        moveA = 'vertical'
+        moveB = 'vertical'
+    if counts[pcvl.BasicState([0,1,0,1])]==1:
+        moveA = 'horizontal'
+        moveB = 'vertical'
+
+    grid.move_drone(drones[0], moveA, 1)
+    grid.move_drone(drones[1], moveB, 1)
+
+def display_grid_and_last_move(grid, drones):
+    clear_console()
+    grid.display_grid()
+
+    for drone in drones:
+        last_move = drone.get_last_move()
+        print(f'Last Move of {drone.name}: {last_move}')
+
+def main():
+    
+    #UI INIT
+    drone_A = Drone('A')
+    drone_B = Drone('B')
+    grid = DroneGrid(2, 2)  # 2x2 grid
+    grid.place_drone(drone_A, 0, 0)
+    grid.place_drone(drone_B, 1, 1)
+    drones = [drone_A, drone_B]
+
+    # CIRCUIT INIT
+    circuit = pcvl.Circuit(4)
+    circuit.add(0, pcvl.BS.H())
+    circuit.add(2, pcvl.BS.H())
+    p = pcvl.Processor("SLOS", circuit)
+    bs1 = pcvl.BasicState([0,1,1,0])
+    bs2 = pcvl.BasicState([1, 0, 0, 1])
+    input_state = bs1-bs2               
+    p.with_input(input_state)
+    sampler = pcvl.algorithm.Sampler(p)
+
+    try:
+        while True:
+            move_drones(grid, drones, sampler)
+            display_grid_and_last_move(grid, drones)
+            time.sleep(3)
+    except KeyboardInterrupt:
+        print("\nScript interrupted.")
+
+if __name__ == "__main__":
+    main()
